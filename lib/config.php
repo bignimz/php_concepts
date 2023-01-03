92e80321c6739816e69ab6edb0ef6a4900ae410b
[alroude] <[nimrod.chitayi@gmail.com]>
2023-01-03 22:57:55 +0300

    added proper folder structure - mvc

--- lib/config.php
+++ lib/config.php
@@ -1,7 +1,7 @@
 <?php
 
 $config = array(
-    'database_dsn' => 'mysql:dbname=dbname;host=localhost',
+    'database_dsn' => 'mysql:dbname=botzi;host=localhost',
     'database_user' => 'root',
     'database_pass' => null,
 );
